8222fed16f9b0449c62af784c0fb5e3a4e06e5d3
samir <samirali@live.dk>
2022-02-17 19:15:43 +0100

    depen

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const express = require('express');
-const https = require('https');
 const fs = require('fs');
 const app = express();
 
 
 //create Server Instance
 //Option 1
-const server = app.listen(5555);
-
 //Option 2
 //const http = require('http');
 //http.createServer(app).listen(5050);
+//To set view Folder
+//app.set('views','./location');
 
 
 
 //registering  view engine
 app.set('view engine','ejs');
-//To set view Folder
-//app.set('views','./location');
-
-console.log(server.address());
 //Setup static public folder
 app.use(express.static('public'));
 
+
+
+
 //Route
 app.get('/',(req,res) => {
-    
     res.render('index');
 });
 
+const server = app.listen((process.env.PORT || '5000'));
